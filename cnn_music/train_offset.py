2b243f0eb315e12e0dfaff70ad088bb6f94b1f40
victor <baessa60@gmail.com>
2021-03-14 21:57:06 -0300

    novo dataset

--- cnn_music/train_offset.py
+++ cnn_music/train_offset.py
@@ -1,16 +1,14 @@
-from numpy import array
-from numpy import hstack
-import numpy
+import pickle, argparse, sys, json, note
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Conv1D
-from tensorflow.keras.layers import MaxPooling1D
-import note
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.layers import Conv1D, MaxPooling1D
+from tensorflow.keras.layers import Dropout
+from datetime import datetime
 from music21 import stream
-import pickle
-import argparse, sys
-import json
+from numpy import hstack
+from numpy import array
+import numpy
+
 
 def split_sequences(sequences, n_steps):
     X, y = list(), list()
@@ -36,8 +34,11 @@ input_parser.add_argument('-mp','--midipath',
         help='path to mid, you can use \*.mid for all midis on directory; dataset/*.mid assumed if not specifyed'
         )
 args = input_parser.parse_args()
-midipath = args.midipath if args.midipath else 'dataset/*.mid'
+midipath = args.midipath if args.midipath else 'dataset/*/*.MID'
 configpath = args.config if args.config else 'configs/config.json'
+
+print("get notes")
+print(datetime.now().time())
 
 data = note.get_notes_info(midipath);
 pitchnames = note.get_pitchnames(data['notes']);
@@ -54,16 +55,42 @@ X, y = split_sequences(dataset, n_steps)
 
 n_features = X.shape[2]
 
+print("Treinando")
+print(datetime.now().time())
+
 model = Sequential()
+
 model.add(Conv1D(filters=parameters['filters'], kernel_size=parameters['kernel_size'], activation=parameters['activation'], input_shape=(n_steps, n_features)))
+model.add(Conv1D(filters=parameters['filters2'], kernel_size=parameters['kernel_size'], activation=parameters['activation'], input_shape=(n_steps, n_features)))
 model.add(MaxPooling1D(pool_size=parameters['pool_size']))
+model.add(Dropout(parameters['dropout1']))
+
+model.add(Dense(parameters['dense_units3'], activation=parameters['activation']))
+
+model.add(Conv1D(filters=parameters['filters'], kernel_size=parameters['kernel_size'], activation=parameters['activation'], input_shape=(n_steps, n_features)))
+model.add(Conv1D(filters=parameters['filters2'], kernel_size=parameters['kernel_size'], activation=parameters['activation'], input_shape=(n_steps, n_features)))
+model.add(MaxPooling1D(pool_size=parameters['pool_size']))
+model.add(Dropout(parameters['dropout2']))
+
+model.add(Dense(parameters['dense_units2'], activation=parameters['activation']))
+
+model.add(Conv1D(filters=parameters['filters'], kernel_size=parameters['kernel_size'], activation=parameters['activation'], input_shape=(n_steps, n_features)))
+model.add(Conv1D(filters=parameters['filters2'], kernel_size=parameters['kernel_size'], activation=parameters['activation'], input_shape=(n_steps, n_features)))
+model.add(MaxPooling1D(pool_size=parameters['pool_size']))
+model.add(Dropout(parameters['dropout3']))
+
 model.add(Flatten())
-model.add(Dense(parameters['dense_units'], activation=parameters['activation']))
+model.add(Dense(parameters['dense_units1'], activation=parameters['activation']))
+model.add(Dropout(parameters['dropout4']))
 model.add(Dense(n_features))
+
 model.compile(optimizer=parameters['optimizer'], loss=parameters['loss'])
-model.fit(X.astype(numpy.float32), y.astype(numpy.float32), epochs=parameters['epochs'], verbose=parameters['verbose'])
+model.fit(X.astype(numpy.float32), y.astype(numpy.float32), epochs=parameters['epochs'], verbose=parameters['verbose'], validation_split=parameters['validation'])
 
 model.save('models/modelOffSet')
+
+print("fim treino")
+print(datetime.now().time())
 
 output_file = open("models/modelOffSett",'wb')
 train_output = {
